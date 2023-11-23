4b9ccc626e1e308c264a363151c9713fa0094574
Shashi kushwaha <shashikumarkushwaha3@gmail.com>
2023-11-23 16:23:56 +0530

    modify

--- test/testProblem1.js
+++ test/testProblem1.js
@@ -1,6 +1,7 @@
 const { inventory } = require("../inventory");
 const { problem1 } = require("../problem1");
 const ID = 33;
+
 const carDetail = problem1(inventory, ID);
 
 console.log(
--- test/testProblem2.js
+++ test/testProblem2.js
@@ -1,9 +1,8 @@
 const { inventory } = require("../inventory");
 const { problem2 } = require("../problem2");
-
 const lastCar = inventory.length - 1;
-const lastCarDetails = problem2(inventory, lastCar);
 
+const lastCarDetails = problem2(inventory, lastCar);
 console.log(
   `Last car is ${lastCarDetails[0].car_make} ${lastCarDetails[0].car_model}`
 );
